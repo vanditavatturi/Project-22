bcec505776e05b381eb3c6fc1843588d9b646889
vanditavatturi <86448572+vanditavatturi@users.noreply.github.com>
2021-07-15 19:52:38 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,15 @@
+class Box{
+    constructor(x,y,width,height){
+        var options = {
+            'restitution': 1.0
+        }
+        this.body = Bodies.rectangle(x,y,width,height);
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("red");
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
